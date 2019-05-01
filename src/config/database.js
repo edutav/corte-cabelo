00470aaeaf714705bd2d9f62b2d88bd9b5e58af2
Eduardo Tavares <edutav@gmail.com>
2019-05-01 17:48:59 -0400

    primeiro commit

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+	dialect: 'mysql2',
+	host: '127.0.0.1',
+	username: 'root',
+	password: 'root',
+	database: 'gobarber',
+	operatorAliases: false,
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true
+	}
+};
